e38d3d05501f89b622ad95af58795b19e0948ec5
willowsys <willowsys@willowsystekiMac.local>
2014-10-22 00:43:09 +0800

    wowjacks do

--- wowjack/main.m
+++ wowjack/main.m
@@ -11,7 +11,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!,wdawd");
+        NSLog(@"Hello, Wwwwwweqweqorld!,wdawd");
     }
     return 0;
 }
